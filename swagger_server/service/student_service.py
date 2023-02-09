6400a1201c41bef4c1d4f2b4c7719359d3a0557e
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2023-02-09 14:57:04 +0100

    MongoDB Call Changes

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -9,8 +9,8 @@ mongoClient = pymongo.MongoClient("mongodb://mongo:27017/")
 mydb = mongoClient["mydatabase"]
 collection = mydb["students"]
 
-db_dir_path = tempfile.gettempdir()
-db_file_path = os.path.join(db_dir_path, "students.json")
+# db_dir_path = tempfile.gettempdir()
+# db_file_path = os.path.join(db_dir_path, "students.json")
 # student_db = TinyDB(db_file_path)
 
 
@@ -20,32 +20,37 @@ def add(student=None):
     # queries.append(query.first_name == student.first_name)
     # queries.append(query.last_name == student.last_name)
     # query = reduce(lambda a, b: a & b, queries)
+    student_id = collection.count_documents({}) + 1
+
     query = { "first_name": student.first_name, "last_name": student.last_name }
     res = collection.count_documents(query)
     #res = student_db.search(query)
     if res > 0:
         return 'already exists', 409
 
-    #doc_id = student_db.insert(student.to_dict())
-    doc_id = collection.insert_one(query).inserted_id
-    student.student_id = str(doc_id)
-    # return str(student.student_id)
-    return student
+    student = student.to_dict()
+    student.update({"student_id": student_id})
+    collection.insert_one(student)
+
+    return student_id
 
 def get_by_id(student_id=None, subject=None):
     #student = student_db.get(doc_id=int(student_id))
     student = collection.find_one({"student_id": student_id})
+
     if not student:
         return 'not found', 404
+
     #print(student)
     student['_id'] = str(student['_id'])
     return student
 
 def delete(student_id=None):
-    student = collection.find({"doc_id": int(student_id)})
+    student = collection.find({"student_id": int(student_id)})
     if not student:
         return 'not found', 404
     #student_db.remove(doc_ids=[int(student_id)])
-    collection.delete_one({"doc_id": int(student_id)})
+    collection.delete_one({"student_id": int(student_id)})
     return student_id
+
 
